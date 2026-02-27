5cdaae462b6b575c3e88566f35011ece002bbc9f
Your Name <you@example.com>
2026-02-27 15:52:13 +0600

    task two complete

---
+++ task-2.js
@@ -0,0 +1,6 @@
+function multiply(a,b,c,d){
+    let multiplicationOfFourNumbers= a*b*c*d;
+    return multiplicationOfFourNumbers;
+}
+const resultOfMultication = multiply(5,45,1,4);
+console.log(resultOfMultication);git 
